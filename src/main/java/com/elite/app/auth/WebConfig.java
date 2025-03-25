7d13dad77efd0c9ab0d6e24a2726c4303b32fc43
DSWAnthony <anthonysistemas159@gmail.com>
2025-03-25 00:24:36 -0500

    changes

--- src/main/java/com/elite/app/auth/WebConfig.java
+++ src/main/java/com/elite/app/auth/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer{
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .maxAge(3600);
